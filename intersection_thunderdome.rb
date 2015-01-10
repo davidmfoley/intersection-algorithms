215762b428bfb6d529a66fdf5eef9f78fac829d3
Dave Foley <davidmfoley@gmail.com>
2015-01-09 22:21:23 -0800

    Clean up thunderdome

--- intersection_thunderdome.rb
+++ intersection_thunderdome.rb
@@ -1,35 +1,39 @@
 #====================================================================
-# Benchmarks for the various implementations in the degenerate case
-# of two arrays that don't intersect at all.
+# Thunderdome!
+#
+# Runs each algorithm on a dataset that doubles in size each iteration
+# Times the results of each and prints out a ranking.
+#
+# Eliminates algorithms that take longer than the specified limit 
+# (see below)
 #====================================================================
-
-# the number of items to use for each pass 
-# YMMV - On my computer 8,000 items takes ~3.5 seconds for the slowest algorithm
 
 require_relative './intersection'
 
+count = 1024 # how many items to start with - this is doubled each round
+limit = 2    # (in seconds) algorithms that take longer than this are eliminated
+
+# start with all of them
 contenders = Intersection.algorithms
 
 puts "THUNDERDOME! " + contenders.length.to_s + ' ALGORITHMS ENTER BUT ONLY ONE LEAVES VICTORIOUS!'
 
-count = 1000
-limit = 2 #(seconds) algorithms that take longer than this are eliminated
-
 # for each count, run all of the algorithms and rank them by time
-while contenders.length > 0
+while contenders.length > 1
   # (count) odd and even numbers
   odds = (1..count*2).step(2).to_a.shuffle
   evens = (2..count*2).step(2).to_a.shuffle
 
-  results = [] 
+  results = []
 
   # run for each implementation
   contenders.each do |contender|
 
-    # you could try with other sets of numbers - all intersecting, for example (set a and b to the same thing)
+    # you could try with other sets of numbers - all intersecting, for example
     # or partially intersecting
     # or random numbers...
-    # or non-numeric value...
+    # or non-numeric values...
+    # or symbols
 
     # We use dup here so that each one is its own copy that is not affected by shift/pop/sort! etc.
     a = odds.dup
@@ -47,28 +51,31 @@ while contenders.length > 0
   results.sort_by! {|x| x[1]}
 
   puts
-  puts 'Thunderdome round with (' + evens.length.to_s + ') items' 
+  puts 'Thunderdome round with (' + evens.length.to_s + ') items'
   puts
 
   results.each do |result|
+    # TBH this is a pretty lazy way to store these results...
     algorithm = result[0]
     elapsed_seconds = result[1]
+
+    # print the results
     puts algorithm.to_s + ': ' + (elapsed_seconds * 1000).to_i.to_s + ' ms'
 
+    # eliminate if too slow
     if (elapsed_seconds > limit)
       contenders.delete algorithm
       puts algorithm.to_s + ' IS ELIMINATED!'
     end
-
   end
 
-  if (contenders.length == 0)
+  # end condition
+  if (contenders.length <= 1)
     puts
     puts "THE WINNER IS " + results[0][0].to_s
   else
     puts contenders.length.to_s + ' contenders remain'
   end
-
 
   count += count
 end
