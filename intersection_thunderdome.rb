974d43fd38681cc3ad0227c6092bd54c9fc4acaf
Dave Foley <davidmfoley@gmail.com>
2015-01-10 11:01:53 -0800

    refactor thunderdome

--- intersection_thunderdome.rb
+++ intersection_thunderdome.rb
@@ -16,53 +16,54 @@ require_relative './intersection'
 count = 1024 # how many items to start with - this is doubled each round
 limit = 2    # (in seconds) algorithms that take longer than this are eliminated
 
+
 # start with all of them
 contenders = Intersection.algorithms
 
 class Result < Struct.new(:algorithm, :elapsed_seconds)
   def to_s
-    self.algorithm.name + ': ' + (self.elapsed_seconds * 1000).to_i.to_s + ' ms'
+    (self.elapsed_seconds * 1000).to_i.to_s + ' ms' + ': ' + self.algorithm.name.split('::').last
   end
 end
 
-# for each count, run all of the algorithms and rank them by time
-while contenders.length > 1
-  # (count) odd and even numbers
-  odds = (1..count*2).step(2).to_a.shuffle
-  evens = (2..count*2).step(2).to_a.shuffle
+def measure_contender_time(contender, array1, array2)
+  # We use dup here so that each one is its own copy that is not affected by shift/pop/sort! etc.
+  # time the intersect operation in "real" time
+  elapsed = Benchmark.measure { contender.intersect(array1.dup, array2.dup) }.real
 
+  Result.new(contender, elapsed)
+end
 
+def measure_all_contenders(contenders, limit, odds, evens)
   results = []
 
   # run for each implementation
   contenders.each do |contender|
+    results << measure_contender_time(contender, odds, evens)
 
-    # you could try with other sets of numbers - all intersecting, for example
-    # or partially intersecting
-    # or random numbers...
-    # or non-numeric values...
-    # or symbols
-
-    # We use dup here so that each one is its own copy that is not affected by shift/pop/sort! etc.
-    a = odds.dup
-    b = evens.dup
-
-    # time the intersect operation in "real" time
-    elapsed = Benchmark.measure { contender.intersect(a, b) }.real
-
-    # Store in an array of arrays [[algorithm, timing]]
-    results << Result.new(contender, elapsed)
     putc '.'
   end
 
   # best (shortest time) results first
   results.sort_by! {|x| x[1]}
+end
 
+
+def do_round(contenders, count, limit)
+  # (count) odd and even numbers
+  # you could try with other sets of numbers - all intersecting, for example
+  # or partially intersecting
+  # or random numbers...
+  # or non-numeric values...
+  # or symbols
+
+  odds = (1..count*2).step(2).to_a.shuffle
+  evens = (2..count*2).step(2).to_a.shuffle
+
+  results = measure_all_contenders(contenders, limit, odds, evens)
   puts
   puts 'Thunderdome round with (' + evens.length.to_s + ') items'
   puts
-
-
 
   results.each do |result|
     # print the results
@@ -83,6 +84,12 @@ while contenders.length > 1
   else
     puts contenders.length.to_s + ' contenders remain'
   end
+end
+
+# for each count, run all of the algorithms and rank them by time
+while contenders.length > 1
+  do_round(contenders, count, limit)
+
 
   count += count
 end
